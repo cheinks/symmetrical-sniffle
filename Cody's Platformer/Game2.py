a8d30ecffb4527078bd90b8f3303f6a4d3a2c383
Cody <cheinks1074@gmail.com>
2019-10-20 14:43:53 -0700

    collisionsssssssssssss

--- Cody's Platformer/Game2.py
+++ Cody's Platformer/Game2.py
@@ -28,23 +28,34 @@ class Sprite(pygame.sprite.Sprite):
 
 
 class Player(Sprite):
-    def __init__(self, graphic, x, y, width, height, strength):
-        Sprite.__init__(self, graphic, x, y, width, height)
+    def __init__(self, graphic, x, y, strength, width=None, height=None):
+        if width is None or height is None:
+            Sprite.__init__(self, graphic, x, y, 20, 20)
+        else:
+            Sprite.__init__(self, graphic, x, y, width, height)
         self.strength = strength
 
         self.x_vel = scroll_speed
         self.y_vel = 0
         self.on_ground = True
-        # self.jump = False
 
     def move(self):
         self.rect.x += self.x_vel
-        self.rect.y += self.y_vel
+        self.rect.y += self.y_vel + gravity
         self.y_vel += gravity
+
+        if self.y_vel != 0:
+            self.on_ground = False
 
     def jump(self):
         if self.on_ground:
-            self.y_vel = self.strength
+            self.y_vel = -self.strength
+            self.on_ground = False
+
+    def land(self, ground_level):
+        self.rect.y = ground_level - self.height
+        self.on_ground = True
+        self.y_vel = 0
 
 
 class Ground(Sprite):
@@ -58,9 +69,14 @@ class Ground(Sprite):
 current_segment = 0
 ground_spawn_rate = 500
 next_ground = 0
-# play speed / scroll speed = ticks per frame
 
 
+def generate_ground_rate():
+    rate = [100, 200, 300, 400, 500, 1000, 1500]
+    return rate[random.randint(0, len(rate) - 1)]
+
+
+# Generate terrain
 def generate_ground():
     x = screen_width
     y = random.randint(270, 810)
@@ -73,6 +89,7 @@ def generate_ground():
 level = []
 for i in range(50):
     level.append(generate_ground())
+
 
 pygame.init()
 
@@ -87,8 +104,14 @@ main_ground = Ground(0, (screen_height / 2), screen_width, (screen_height / 2), 
 Obstacles = pygame.sprite.Group()
 Obstacles.add(main_ground)
 
+jump_strength = 50
+player1 = Player('player.png', 500, 500, jump_strength)
+Players = pygame.sprite.Group()
+Players.add(player1)
+
 All_Sprites = pygame.sprite.Group()
 All_Sprites.add(main_ground)
+All_Sprites.add(player1)
 
 playing = True
 
@@ -97,23 +120,55 @@ while playing:
 
     for event in pygame.event.get(pygame.QUIT):
         playing = False
+
     for event in pygame.event.get(pygame.KEYDOWN):
         if event.key == pygame.K_ESCAPE:
             playing = False
+        if event.key == pygame.K_SPACE:
+            player1.jump()
+
+    for event in pygame.event.get(pygame.KEYUP):
+        pass
+    pygame.event.clear()
 
     if timer >= next_ground:
-        next_ground = timer + ground_spawn_rate
         All_Sprites.add(level[current_segment])
         Obstacles.add(level[current_segment])
+
+        next_ground = timer + ground_spawn_rate
+        # ground_spawn_rate = generate_ground_rate()
         current_segment += 1
 
+    # Move all of the sprites
     for each_sprite in All_Sprites:
         each_sprite.scroll()
+    for each_player in Players:
+        each_player.move()
+
+    # Check collisions
+    gravity_check = pygame.sprite.groupcollide(Players, Obstacles, False, False)
+    for each_player in gravity_check:
+        collided_with = gravity_check[each_player]
+
+        i = 0
+        while i < len(collided_with):
+            each_obstacle = collided_with[i]
+            bottom = each_player.rect.y + each_player.height
+            print(bottom)
+            print(each_obstacle.rect.y)
+            print()
+            if bottom > each_obstacle.rect.y:
+                player1.kill()
+                i = len(collided_with)
+            else:
+                each_player.land(each_obstacle.rect.y)
+            i += 1
 
     main_screen.blit(main_background, [0, 0])
     main_screen.blit(main_ground.image, [main_ground.rect.x, main_ground.rect.y])
 
     Obstacles.draw(main_screen)
+    Players.draw(main_screen)
 
     pygame.display.flip()
     clock.tick(play_speed)
